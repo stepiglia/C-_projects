260b140677771b49a5b542a80316a60cc5e7adf8
stepiglia <stefano.pigliafreddo@gmail.com>
2020-12-27 15:01:52 +0100

    Update mediana con puntatori.cpp

--- lists/mediana con puntatori.cpp
+++ lists/mediana con puntatori.cpp
@@ -1,37 +1,7 @@
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@stepiglia 
-stepiglia
-/
-Cpp_uni_projects
-Private
-1
-00
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-Settings
-Cpp_uni_projects/lists/mediana con puntatori.cpp
-@stepiglia
-stepiglia Create mediana con puntatori.cpp
-Latest commit 09a4d6d 4 hours ago
- History
- 1 contributor
-97 lines (74 sloc)  2.4 KB
-  
 /*
   Esercitazione 16
   Calcolo Mediana con puntatori
+
 */
 
 #include <iostream>
@@ -44,7 +14,7 @@ struct Nodo
 };
 
 // prototipi
-int calcolaMediana(Nodo *testa); 
+int calcolaMediana(Nodo *testa);
 void inserimento(Nodo * &testa, int numero);
 
 int main(void)
@@ -52,7 +22,7 @@ int main(void)
         int numero;
         int n;
 
-        Nodo *lista=0;
+        Nodo *lista=0; //Inizializzo la lista a zero
 
         cout << endl << "Quanti numeri si vogliono inserire nella lista?";
         cin >> n;
@@ -70,33 +40,33 @@ int main(void)
 
 void inserimento(Nodo * &testa, int numero)
 {
-        Nodo *ptr=testa;
-        while ((ptr!=0)&&(numero!=ptr->valore))
-              ptr=ptr->nextPtr;
+        Nodo *ptr=testa;  //testa è di tipo puntatore a nodo
+        while ((ptr!=0)&&(numero!=ptr->valore)) //fin tanto che puntatore non è zero e il numero è diverso dal puntatore di valore
+              ptr=ptr->nextPtr; //il puntatore diventa il puntatore al nuovo oggetto
 
-        if (ptr==0){
-           Nodo *temp=new Nodo; 
-           temp->valore=numero;
-           temp->nextPtr=testa;
-           testa=temp;
-	}
+        if (ptr==0){ //se il puntatore è zero
+           Nodo *temp=new Nodo; //creo un nuovo nodo temp
+           temp->valore=numero; //assegno il numero
+           temp->nextPtr=testa; //e lo faccio puntare alla testa della lista
+           testa=temp; //che non è altro che temp
+    }
 }
 
-int calcolaMediana(Nodo *testa) 
+int calcolaMediana(Nodo *testa)
 {
         Nodo *ptr1;     //puntatore ad un nodo della lista
-        int mediana,    //valore della mediana che verr‡ restituito
+        int mediana,    //valore della mediana che verrà restituito
                         //dalla funzione
             minori, maggiori;
 
         int n = 0;      //numero di elementi contenuti nella lista
         
         //conteggio del numero di elementi contenuti nella lista
-        for(ptr1 = testa; ptr1 != 0; ptr1 = ptr1->nextPtr) 
+        for(ptr1 = testa; ptr1 != 0; ptr1 = ptr1->nextPtr) //scorro la lista
                 n++;
 
         ptr1 = testa;    //posizionamento all'inizio della lista
-					    // con un secondo puntatore
+                        // con un secondo puntatore
         do {
 
            //Conteggio di quanti elementi dell'insieme
@@ -105,7 +75,7 @@ int calcolaMediana(Nodo *testa)
                 minori = 0;
                 maggiori = 0;
 
-                //scansione della lista per contare i minori ed i 
+                //scansione della lista per contare i minori ed i
                 //maggiori
                 for (Nodo *ptr2 = testa; ptr2 != 0;
                      ptr2 = ptr2->nextPtr)
@@ -115,13 +85,13 @@ int calcolaMediana(Nodo *testa)
                      else if (ptr1->valore < ptr2->valore)
                          maggiori++;
 
-                mediana= ptr1->valore; //salva il valore prima di 
-                                       //procedere  
+                mediana= ptr1->valore; //salva il valore prima di
+                                       //procedere
 
                 ptr1 = ptr1->nextPtr;  //nodo successivo
 
-        } while (!(((minori    <= (n - 1)/2) && (maggiori <= n/2))  
+        } while (!(((minori    <= (n - 1)/2) && (maggiori <= n/2))
                    ||((maggiori <= (n - 1)/2) && (minori    <= n/2))));
-		
+        
         return mediana;
 }
