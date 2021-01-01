9866ea8b5c3df816482822d679ba29174b1abc90
stepiglia <stefano.pigliafreddo@gmail.com>
2021-01-01 11:15:54 +0100

    Update reference.cpp

--- reference.cpp
+++ reference.cpp
@@ -2,7 +2,15 @@
 
 class StefClass{
 //public --> means that you can use it outside of the class
+  
+  //-------PUBLIC-------- 
 public: 
+  
+  StefClass(string z){
+    setName(z);
+    cout << "This will get printed as soon as you call the object"; 
+  } //the costuctor name is always the same as the class name . It is useful when you want to 
+  
   void coolSaying(){
     cout << "ciao << endl; 
   }
@@ -15,7 +23,8 @@ public:
   string getName(){ //and to get the value of the variable we use this other function 
     return name; 
   }
-  
+
+  //-------PRIVATE--------
 private:
   string name2; //you make the variable private and instead you make the function that accesses it public in order to use it 
   
@@ -26,7 +35,54 @@ private:
 // Because you could have a function with the same name in multiple classes 
 
 int main(){
-  StefClass StefObject; //we are making an object of that class
-  StefObject.coolSaying(); 
+  StefClass obj; //we are making an object of that class + the constuctor will run 
+  obj.coolSaying(); 
   return 0; 
+  
+  obj.setName("Sir Stef");
+  cout << obj.getName(); //we need to do this because remember we don't have direct access to the variable 
+ 
+   StefClass obj("")
+  
 }
+
+//-------CONSTRUCTORS--------
+//it's a function that gets called  automatically as soon as you 
+//create an object 
+
+
+//-------SWITCH--------
+switch(age) {
+  case 16: 
+    cout << "Hey you can drive now!" << endl; 
+    break;
+  case 18: 
+    cout << "Get lotto tickets!" << endl; 
+    break;
+  case 21: 
+    cout << "Buy me some beer!" << endl; 
+    break;
+  default: 
+    cout << "Sorry you get nothing" << endl; 
+}
+
+//-------FUNCTION OVERLOADING--------
+//it's the idea of bulding more than one function with the same name 
+//that take different types of data depending on what you take in
+void printNumber(int x){
+  cout << "i am printing an integere" << x << endl; 
+}
+
+void printNumber(float x){
+  cout << "i am printing a float" << x << endl; 
+}
+  
+int a = 54; 
+float b = 21.4869; 
+
+printNumber(a); 
+printNumber(b); 
+
+//that's why you have to declare the type of variables you pass into functions btw :)
+
+
