97a90bcee94286813c0d5b926a84a375d9582f6d
stepiglia <stefano.pigliafreddo@gmail.com>
2021-01-03 21:38:19 +0100

    Update ricerca numero.cpp

--- arrays/ricerca numero.cpp
+++ arrays/ricerca numero.cpp
@@ -13,23 +13,27 @@
 #include <iomanip>
 #include <iostream>
 #include <cstdlib>
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <string.h>
+
 using namespace std;
 
 
-int const R = 6;
-int const C = 8;
-
 //----VARIABILI
-int individuaSequenza(int matrice[R][C], int ins_numero[]);
+//int individuaSequenza(int matrice[R][C], int ins_numero[]);
 
 //----PROTOTIPI FUNZIONI
 
 //----MAIN
-int main (){
+int main(void){
     
-    int N = 0;
+    int const R = 6;
+    int const C = 8;
     
-    int matrice[R][C] = {
+    int A[4] = {4,2,1,3};
+    int B[R][C] = {
         {7,3,5,4,2,1,3,8},
         {9,6,5,3,4,4,2,1},
         {3,7,8,1,5,6,0,2},
@@ -38,62 +42,23 @@ int main (){
         {6,3,1,2,4,7,7,0},
     };
     
-    cout << "Di quante cifre è il numero che stai cercando?" << endl;
-    cin >> N;
-    
-    int ins_numero[N];
-    
-    cout << "Inserisci il numero intero che vuoi cercare" << endl;
-   
-    int i = 0;
-    for(i=0; i<N; i++){
-        cout << "Inserisci la cifra : " << i << "-esima" <<endl;
-        cin >> ins_numero[i];
-    };
+    size_t i, j, size, rows, cols;
+    int founded = 0;
 
-    individuaSequenza(matrice,ins_numero);
-    
-   
-}; //end main
-
-//----FUNZIONI
-
-int individuaSequenza(int matrice[R][C], int ins_numero[]){
-    
-    int contatore = 0;
-    int i = 0, j=0, z = j -(C+1)*((j-2)/C), y = 0;
-    
-        for (z=0; z<C*R; z++) {
-        //casi in cui andiamo oltre al bordo
-     
-        
-            
-        //prima riga prima colonna
-        if(ins_numero[0]/matrice[i+y][j+z] == 1){
-            if(ins_numero[1]/matrice[i+y][j+z] == 1);
-                if(ins_numero[1]/matrice[i+y][j+z] == 1);
-                    if(ins_numero[1]/matrice[i+y][j+z] == 1);
+    size = sizeof(A) / sizeof(A[0]);
+    rows = sizeof(B) / sizeof(B[0]);
+    cols = sizeof(B[0]) / sizeof(B[0][0]);
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols - size; j++) {
+            if (memcmp(A, &B[i][j], sizeof(A)) == 0) {
+                founded = 1;
+                break;
+            }
         }
-           
-           
-           //FORMULA 
-            
-            
-            
-            //se giusto controllo il secondo numero
-            
-            //altrimenti incremento la cella
-            
-            
-        if ()
-            cout << "qui" << matrice[i+y][j+z] << endl;
-            
-           };
-       // };
-    //controlla seconda cifra
+        if (founded) break;
+    }
+    if (founded) printf("Row: %zu Col: %zu\n", i, j);
     return 0;
-}; //end fun
+};
 
 
-//Si scriva, in linguaggio C++, la funzione individuaSequenza che, avendo come parametri una //matrice di interi ad una sola cifra (0-9) di dimensione MxN ed un numero intero, //restituisca come valore di ritorno il numero di volte in cui ha trovato le cifre del //numero passato come parametro, prese nell’ordine destrorso o sinistrorso lungo la sola //direzione orizzontale; la funzione deve inoltre stampare a video e su un file //appositamente creato, le coordinate della cella a partire dalla quale è stata //individuata la sequenza ed il suo verso. Ogni sequenza individuata non dovrà //necessariamente appartenere alla stessa riga.
-//Ad es. passati come parametri l’intero 4213 e la seguente matrice
