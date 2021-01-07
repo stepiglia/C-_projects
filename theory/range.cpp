49da5b73a08cc9f7e8dd0ace60995855001b0d96
stepiglia <stefano.pigliafreddo@gmail.com>
2021-01-07 11:22:56 +0100

    Create range.cpp

---
+++ theory/range.cpp
@@ -0,0 +1,15 @@
+//https://www.youtube.com/watch?v=x0nP_IoUDoM&list=PL0qAPtx8YtJfZpJD7uFxAXglkiHSEhktG&index=56
+#include <iostream> 
+using namespace std;
+
+int main(){
+
+int arrayDiInteri[100];
+
+for (int ele : arrayDiInteri) 
+  ele = rand(); 
+  
+  //PRO: più veloce se devo iterare su tutti i valori 
+  //CONTRO: minore flessibilità, non posso iterare e saltare elementi per esempio
+  
+}
